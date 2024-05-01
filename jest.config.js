fe74075c4396eb83b36b62153b850be62e79e761
Rayhand Viantama <vian.117.bls@gmail.com>
2024-05-01 02:56:27 +0000

    up project

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const 
+
+config = {
+  verbose: true,
+};
+
+// module.exports = config;
+
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+  },
+}
